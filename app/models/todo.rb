2bf1b7a2549cc073ab9ec43fed62a2a8f8888721
roandayne <roandaynedenaerys@gmail.com>
2025-01-31 12:23:16 +0800

    Add validations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,14 @@
 class Todo < ApplicationRecord
+
+  # Constant values
+  STATUSES = ['incomplete', 'doing', 'complete', 'pending']
+
+  # Validations
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :status, inclusion: { in: STATUSES, message: "%{value} is not a valid status" }
+  validates :position, numericality: { only_integer: true }
+
+  # Default scope
+  default_scope { order(position: :asc) }
 end
