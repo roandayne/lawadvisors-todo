0ad427b1ed145b608bd91a455aa760f539efd7b6
roandayne <roandaynedenaerys@gmail.com>
2025-01-31 14:00:51 +0800

    Add serializer to  hide the created, delete, and update at

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  private
+
+  def record_not_found(exception)
+    render json: { error: "Record not found: #{exception.message}" }, status: :not_found
+  end
 end
